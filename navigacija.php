51e7aff107c527bb4691ce68647f8043f33ab5da
Almin Halilović <ahalilovic5@etf.unsa.ba>
2015-05-14 12:33:20 +0200

    Create navigacija.php

---
+++ navigacija.php
@@ -0,0 +1,6 @@
+<ul>
+<li><a onclick="ajaxMenu('index.php')">Naslovnica </a> </li>
+<li > <a  onclick="ajaxMenu('proizvodi.php')"> Proizvodi </a> </li>
+<li > <a  onclick="ajaxMenu('onama.php')"> O nama </a> </li>
+<li > <a  onclick="ajaxMenu('kontakt.php')">Kontakt </a> </li>
+</ul>
