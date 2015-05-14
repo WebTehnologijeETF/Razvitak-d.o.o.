2e5db58eec5c654c2e6ab0da1d879e4feaddc314
Almin Halilović <ahalilovic5@etf.unsa.ba>
2015-05-14 12:44:29 +0200

    Update potvrdaPodataka.php

--- potvrdaPodataka.php
+++ potvrdaPodataka.php
@@ -4,11 +4,7 @@ $ime = $_POST['ime'];
 $mail = $_POST['mail'];
 $message = $_POST['textArea'];
 $firm = $_POST['firm'];
-<<<<<<< HEAD
 echo          '<div id="confirmationDiv">';
-=======
-
->>>>>>> origin/master
 echo		 '<p class="h2Style">Provjerite da li ste ispravno unijeli podatke:</p>';
 echo         '<p class="djelatnost">Ime: ' .$ime. '</p>';
 echo         '<p class="djelatnost">Email: ' .$mail. '</p>';
@@ -17,7 +13,6 @@ echo         '<p class="djelatnost">Firma: ' .$firm. '</p>';
 echo 		 '<form class="formElements" method="post" action="sendEmail.php">';
 echo            '<input type="hidden" name="ime" value="' . $ime . '">';
 echo            '<input type="hidden" name="mail" value="' . $mail . '">';
-<<<<<<< HEAD
 echo            '<input type="hidden" name="firm" value="' . $firm . '">';
 echo            '<input type="hidden" name="textArea" value="' . $message . '">';
 
@@ -25,11 +20,4 @@ echo         	'<input type="submit" id="siguranButton" value="Siguran sam"><br>'
 echo         '</form>';
 echo 		 '<p class="h2Style">Ako ste pogrešno unijeli podatke, ovdje ih možete ispraviti:</p>';
 echo         '</div>';
-=======
-echo            '<input type="hidden" name="textArea" value="' . $message . '">';
-echo            '<input type="hidden" name="firm" value="' . $firm . '">';
-echo         	'<input type="submit" id="siguranButton" value="Siguran sam"><br>';
-echo         '</form>';
-echo 		 '<p class="h2Style">Ako ste pogrešno unijeli podatke, ovdje ih možete ispraviti:</p>';
->>>>>>> origin/master
 ?>
