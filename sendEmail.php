5faffc9b4c50511f02b0039e97c95d3172fb8ebd
Almin Halilović <ahalilovic5@etf.unsa.ba>
2015-05-14 12:45:24 +0200

    Update sendEmail.php

--- sendEmail.php
+++ sendEmail.php
@@ -1,6 +1,5 @@
 <?php
 $to = "ahalilovic5@etf.unsa.ba";
-<<<<<<< HEAD
 $subject = "Razvitak-poruka";
 $ime = strip_tags($_POST['ime']);
 $mail = strip_tags($_POST['mail']);
@@ -8,11 +7,6 @@ $message = strip_tags($_POST['textArea']);
 $firm = strip_tags($_POST['firm']);
 $txt = 'Ime: ' . $ime . '\n' . 'Email: ' . $mail . '\n' . 'Firma: ' . $firm . '\n' . 'Poruka: ' . $message;
 $headers = ' From:' .$mail .'CC: almin.halilovic@hotmail.com' . '\r\n' . 'Reply-To: ' . $message;
-=======
-$subject = "Test poruka - kontakt forma";
-$txt = 'Ime: ' . $_POST['ime'] . '\n' . 'Email: ' . $_POST['mail'] . '\n' . 'Firma: ' . $_POST['firm'] . '\n' . 'Poruka: ' . $_POST['textArea'];
-$headers = 'CC: almin.halilovic@hotmail.com' . '\r\n' . 'Reply-To: ' . $_POST['mail'];
->>>>>>> origin/master
 $isMailSent = mail($to,$subject,$txt,$headers);
 echo '<script>' . 'alert("' . $isMailSent . '")' . '</script>';
 ?>
