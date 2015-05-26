889a68d2d3f0da73f9ba5aeea21184bd9163a868
Almin Halilović <ahalilovic5@etf.unsa.ba>
2015-05-26 20:23:44 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<HTML>
+<HEAD>
+<TITLE>Naslovnica</TITLE>
+<link rel="stylesheet" type="text/css" href="stylesheet.css">
+<META http-equiv="Content-Type" content="text/html; charset=utf-8">
+<script src="skripta.js" type="text/javascript"> </script>
+</HEAD>
+<BODY>
+
+<div id="logo">
+<img src="slike/logo.png" alt="logo">
+
+</div>
+
+<div id="navigation">
+<?php include('navigacija.php');?>
+</div>
+
+<div class="introduction" >
+<h1> Dobrodošli na službenu stranicu firme Razvitak d.o.o. </h1>
+<h2> Stranica je trenutno u procesu razvoja </h2>
+</div>
+
+<?php include 'novosti.php';?>
+
+
+
+<div id="footer"><p>Copyright &copy; Almin Halilović</p></div>
+
+</BODY
+</HTML>
