a42ffb113fca92c4ce745aaa976742fcab1216b6
Almin Halilović <ahalilovic5@etf.unsa.ba>
2015-05-26 20:26:47 +0200

    Create potvrdaPodataka.php

---
+++ potvrdaPodataka.php
@@ -0,0 +1,23 @@
+<?php
+
+$ime = htmlentities($_POST['ime'],ENT_QUOTES);
+$mail = htmlentities($_POST['mail'],ENT_QUOTES);
+$message = htmlentities($_POST['textArea'],ENT_QUOTES);
+$firm = $_POST['firm'];
+echo          '<div id="confirmationDiv">';
+echo		 '<p class="h2Style">Provjerite da li ste ispravno unijeli podatke:</p>';
+echo         '<p class="djelatnost">Ime: ' .$ime. '</p>';
+echo         '<p class="djelatnost">Email: ' .$mail. '</p>';
+echo         '<p class="djelatnost">Poruka: ' .$message. '</p>';
+echo         '<p class="djelatnost">Firma: ' .$firm. '</p>';
+echo 		 '<form class="formElements" method="post" action="sendEmail.php">';
+echo            '<input type="hidden" name="ime" value="' . $ime . '">';
+echo            '<input type="hidden" name="mail" value="' . $mail . '">';
+echo            '<input type="hidden" name="firm" value="' . $firm . '">';
+echo            '<input type="hidden" name="textArea" value="' . $message . '">';
+
+echo         	'<input type="submit" id="siguranButton" value="Siguran sam"><br>';
+echo         '</form>';
+echo 		 '<p class="h2Style">Ako ste pogrešno unijeli podatke, ovdje ih možete ispraviti:</p>';
+echo         '</div>';
+?>
