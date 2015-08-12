a0f6f7a16d112a9e80849e496755c7f8f9fe09ef
Almin Halilović <ahalilovic5@etf.unsa.ba>
2015-08-12 13:07:37 +0200

    Delete navigacija.php

--- navigacija.php
+++
@@ -1,7 +0,0 @@
-<ul>
-<li><a onclick="ajaxMenu('index.php')">Naslovnica </a> </li>
-<li > <a  onclick="ajaxMenu('proizvodi.php')"> Proizvodi </a> </li>
-<li > <a  onclick="ajaxMenu('onama.php')"> O nama </a> </li>
-<li > <a  onclick="ajaxMenu('kontakt.php')">Kontakt </a> </li>
-<li > <a  onclick="ajaxMenu('prijava.php')">Prijava </a> </li>
-</ul>
